eb297b90c3a6640dc3a6f8ae7ce83ce659097385
Peter Tillemans <pti@snamellit.com>
2010-06-10 09:41:49 +0200

    Authentication and Authorization is in. Navigation starts to work.

--- src/blog/core.clj
+++ src/blog/core.clj
@@ -5,43 +5,6 @@
         ring.adapter.jetty)
   (:require [compojure.route :as route]))
 
-(defn calc [a]
-  (str "<h1>a = " a "</h1>"))
-
-(defn calc-add [x y]
-  (let [a (Integer. x) b (Integer. y)]
-	(str "<h1>" x " + " y " = " (+ a b) "</h1>")))
-
-(defroutes example
-  (GET "/" [] "<h1>Hello World Wide Web!</h1>")
-  (GET "/"
-       (render (index)))
-  (GET "/a/"
-       (render (viewa params session)))
-  (GET "/b/"
-       (render (viewb params session)))
-  (GET "/c/"
-       (render (viewc params session)))
-  (GET "/c/:action"
-       (render (viewc params session)))
-
-  ;; static files
-  (GET "/base.html"
-       (serve-file *webdir* "base.html"))
-  (GET "/3col.html"
-       (serve-file *webdir* "3col.html"))
-  (GET "/navs.html"
-       (serve-file *webdir* "navs.html"))
-  (GET "*/main.css"
-       (serve-file *webdir* "main.css"))
-
-  (GET (str "/calc" "/:a" ) [a] 
-    (calc a))
-  (GET (str "/calc" "/:a" "/add" "/:b") [a b]
-    (calc-add a b))
-
-  (route/not-found "Page not found"))
-
 ;;(run-jetty example {:port 8080})
 	
 ;; (run-jetty example {:port 8080 :ssl true :ssl-port 8443 :keystore "keystore" :key-password "secret"})
