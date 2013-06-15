af4af9b779a84ba4f6fb8ec8323b98dc9fc0c1a4
Peter Tillemans <pti@snamellit.com>
2013-06-15 09:31:35 -0400

    checkin b4 merge

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :dependencies [[org.clojure/clojure "1.2.0-beta1"]
                  [org.clojure/clojure-contrib "1.2.0-beta1"]
                  [compojure "0.4.1"]
-		 [enlive "1.0.0-SNAPSHOT"]
+                 [enlive "1.0.0-SNAPSHOT"]
                  [ring/ring-jetty-adapter "0.2.5"]
                  [org.clojars.bmabey/congomongo "0.1.2-SNAPSHOT"]
                  [rhino/js "1.7R2"]]
   :dev-dependencies [[autodoc "0.7.1"]
-		     [swank-clojure "1.2.1"]]
+                     [swank-clojure "1.2.1"]]
   :main blog.core)
